5f82b068fc4b197598c61190eb60f1422c0b4ca1
Tuyishimire-lab <tuyishime1angel@gmail.com>
2024-07-29 16:28:35 +0200

    Added new styling to the event by  converting it into a react-boostrap card

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Col, Button, Modal } from 'react-bootstrap';
+import { Col, Button, Modal, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import CountDownTimer from './CountDownTimer';
 import { FaEdit, FaTrash, FaEye, FaCalendar } from 'react-icons/fa';
@@ -23,32 +23,40 @@ const Event = ({ event, handleEdit, deleteEvent, navigateToEvent }) => {
 
   return (
     <Col >
-        <div style={{ borderLeft: `5px solid ${event.color}`,width: '350px', padding: '10px', margin: '10px 0', height: '250px', overflow: 'hidden', position: 'relative' }}>
-            <h4>{event.title}</h4>
-            <p><strong>Start:</strong> {new Date(event.startDate).toLocaleString()}</p>
-            <p><strong>End:</strong> {new Date(event.endDate).toLocaleString()}</p>
-            <CountDownTimer startDate={event.startDate} endDate={event.endDate} isRecurring={event.isRecurring} recurrencePattern={event.recurrencePattern}/>
-            <p>{event.description.length > 50 ? `${event.description.substring(0, 50)}...` : event.description}</p>
-            <div style={{ position: 'absolute', bottom: '10px', width: '100%' }}>
-                <Button variant="secondary" onClick={() => handleEdit(event)} style={{ marginRight: '10px' }}><FaEdit/></Button>
-                <Button variant="danger" onClick={() => deleteEvent(event.id)} style={{ marginRight: '10px' }}><FaTrash/></Button>
-                <Button variant="primary" onClick={handleShow}><FaEye/></Button>
-            </div>
-        </div>
 
-        <Modal show={showModal} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>{event.title}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <p><strong>Start:</strong> {new Date(event.startDate).toLocaleString()}</p>
-                <p><strong>End:</strong> {new Date(event.endDate).toLocaleString()}</p>
-                <p>{event.description}</p>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleNavigation}><FaCalendar/></Button>
-            </Modal.Footer>
-        </Modal>
+        <Card style={{ margin: '10px', marginLeft:'1px',marginRight: '1px' ,width: '325px', height: '250px', borderLeft: `5px solid ${event.color}` }}>
+            <Card.Body>
+                <Card.Title>{event.title}</Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>
+                    <p><strong>Start:</strong> {new Date(event.startDate).toLocaleString()}</p>
+                    <p><strong>End:</strong> {new Date(event.endDate).toLocaleString()}</p>
+                </Card.Subtitle>
+                <CountDownTimer startDate={event.startDate} endDate={event.endDate} isRecurring={event.isRecurring} recurrencePattern={event.recurrencePattern}/>
+                <Card.Text>
+                    <p>{event.description.length > 50 ? `${event.description.substring(0, 50)}...` : event.description}</p>
+                </Card.Text>
+        
+                <div style={{ position: 'absolute', bottom: '10px', width: '100%' }}>
+                    <Button variant="secondary" onClick={() => handleEdit(event)} style={{ marginRight: '10px' }}><FaEdit/></Button>
+                    <Button variant="danger" onClick={() => deleteEvent(event.id)} style={{ marginRight: '10px' }}><FaTrash/></Button>
+                    <Button variant="primary" onClick={handleShow}><FaEye/></Button>
+                </div>
+            </Card.Body>
+            <Modal show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{event.title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p><strong>Start:</strong> {new Date(event.startDate).toLocaleString()}</p>
+                    <p><strong>End:</strong> {new Date(event.endDate).toLocaleString()}</p>
+                    <p>{event.description}</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleNavigation}><FaCalendar/></Button>
+                </Modal.Footer>
+            </Modal>
+        </Card>
+
     </Col>
   );
 };
