8dcc91e99a1ef0443d41ab5a219434676af5d724
Tuyishimire-lab <tuyishime1angel@gmail.com>
2024-08-10 05:18:53 +0200

    Re styled the calendar made it more responsive

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,15 +1,14 @@
-import React, {forwardRef, useContext, useImperativeHandle, useRef} from 'react';
+import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import bootstrapPlugin from '@fullcalendar/bootstrap';
 import { EventsContext } from '../EventsContext';
 
-
 const Calendar = forwardRef((props, ref) => {
-
   const { events } = useContext(EventsContext);
-  const calendarRef = useRef(null)
+  const calendarRef = useRef(null);
 
   const handleDateClick = (arg) => {
     console.log(arg.dateStr);
@@ -21,11 +20,10 @@ const Calendar = forwardRef((props, ref) => {
       const event = events.find((e) => e.id === eventId);
       if (event) {
         calendarApi.gotoDate(event.startDate);
-        calendarApi.select({ start: event.startDate, end: event.endDate, allDay: event.allDay }); // Navigate to the event date
+        calendarApi.select({ start: event.startDate, end: event.endDate, allDay: event.allDay });
       }
     },
   }));
-
 
   const formattedEvents = events.map(event => ({
     id: event.id,
@@ -34,25 +32,29 @@ const Calendar = forwardRef((props, ref) => {
     end: event.endDate,
     color: event.color,
     backgroundColor: event.color,
-    borderColor: event.color, 
+    borderColor: event.color,
   }));
 
-  
-
   return (
-    <FullCalendar
-      ref={calendarRef}
-      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-      initialView="dayGridMonth"
-      events={formattedEvents}
-      dateClick={handleDateClick}
-    //   themeSystem='bootstrap'
-      headerToolbar={{
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-      }}
-    />
+    <div className="calendar-container">
+      <FullCalendar
+        ref={calendarRef}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, bootstrapPlugin]}
+        initialView="dayGridMonth"
+        events={formattedEvents}
+        dateClick={handleDateClick}
+        themeSystem='bootstrap'
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }}
+        
+        height="auto"
+        contentHeight="auto"
+        aspectRatio={1.5}
+      />
+    </div>
   );
 });
 
