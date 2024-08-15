fbee5ba9a1fa9c9f5aa6614df305f7f0961a9201
Tuyishimire-lab <tuyishime1angel@gmail.com>
2024-08-15 11:13:23 +0200

    changed from the fullcallender to reactbigcalendar for better styling and agenda feature

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,26 +1,22 @@
 import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import bootstrapPlugin from '@fullcalendar/bootstrap';
+import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 import { EventsContext } from '../EventsContext';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
 
 const Calendar = forwardRef((props, ref) => {
   const { events } = useContext(EventsContext);
   const calendarRef = useRef(null);
 
-  const handleDateClick = (arg) => {
-    console.log(arg.dateStr);
-  };
-
   useImperativeHandle(ref, () => ({
     navigateToEvent: (eventId) => {
-      const calendarApi = calendarRef.current.getApi();
       const event = events.find((e) => e.id === eventId);
       if (event) {
-        calendarApi.gotoDate(event.startDate);
-        calendarApi.select({ start: event.startDate, end: event.endDate, allDay: event.allDay });
+        const calendarApi = calendarRef.current;
+        // Use navigate to go to the event's start date
+        calendarApi.props.onNavigate(event.start);
       }
     },
   }));
@@ -28,31 +24,29 @@ const Calendar = forwardRef((props, ref) => {
   const formattedEvents = events.map(event => ({
     id: event.id,
     title: event.title,
-    start: event.startDate,
-    end: event.endDate,
+    start: new Date(event.startDate),
+    end: new Date(event.endDate),
+    allDay: event.allDay,
     color: event.color,
-    backgroundColor: event.color,
-    borderColor: event.color,
   }));
+
+  const eventPropGetter = (event) => {
+    const backgroundColor = event.color;
+    return { style: { backgroundColor } };
+  };
 
   return (
     <div className="calendar-container">
-      <FullCalendar
+      <BigCalendar
         ref={calendarRef}
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, bootstrapPlugin]}
-        initialView="dayGridMonth"
+        localizer={localizer}
         events={formattedEvents}
-        dateClick={handleDateClick}
-        themeSystem='bootstrap'
-        headerToolbar={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay'
-        }}
-        
-        height="auto"
-        contentHeight="auto"
-        aspectRatio={1.5}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 600 }}
+        eventPropGetter={eventPropGetter}
+        selectable
+        onSelectEvent={(event) => console.log(event)}
       />
     </div>
   );
